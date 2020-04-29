0740017fa1a290b71d075f600d7027908a00042d
MalondaClement <clementmalonda@yahoo.fr>
2020-04-29 19:56:18 +0200

    Ajout de la génération de la courbe de résultat dans le main

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
 from pathwidth import *
 from width import *
 from graphGen import *
@@ -27,13 +30,33 @@ def main(G, dico, fichier):
     fichier.write(" et width = ")
     fichier.write(str(w))
     print("Fait")
+    return pw, w
 
 if __name__ == "__main__":
 
     fichier = open("data.txt", "a")
-    for i in range(5,25):
+    size = []
+    res_moy_pw = []
+    res_moy_w = []
+
+    for i in range(5,100):
+        fichier.write("\n")
+        tmp_pw = []
+        tmp_w = []
         for j in range(15):
             G, dict = newGraph(i, i*3)
-            main(G, dict, fichier)
+            pw, w = main(G, dict, fichier)
+            tmp_pw.append(pw)
+            tmp_w.append(w)
+
+        size.append(i)
+        res_moy_pw.append(np.mean(tmp_pw))
+        res_moy_w.append(np.mean(tmp_w))
+
+    plt.plot(size, res_moy_pw)
+    plt.plot(size, res_moy_w)
+    plt.xlabel('Nombre de sommets dans le graphe')
+    plt.legend(['pw','w'])
+    plt.show()
 
     fichier.close
